fce0b5b13606a2b22179b1480ce8e663546d2422
Pratish Kumar <prkum@microsoft.com>
2023-08-22 17:55:57 -0700

    Aded UI Fixes

--- tumblingtoads/src/App.js
+++ tumblingtoads/src/App.js
@@ -4,10 +4,13 @@ import travel from './blog/travel.json';
 import Topbar from './Topbar';
 import Content from './Content';
 import { useState, useEffect } from 'react';
+import Blogs from './Blogs';
 
 function App() {
   const [currentPost, setCurrentPost] = useState(null);
 
+  let content = currentPost == null ? <Blogs notifyParent={(post) => {setCurrentPost(post)}} posts={travel}></Blogs> : <Content post={currentPost}/>;
+  
   return (
     <div className="Page">
       <Topbar 
@@ -16,8 +19,7 @@ function App() {
         }}
         posts={travel}
       />
-      <Content 
-        post={currentPost == null ? travel[0] : currentPost}/>
+      {content}
     </div>  
   );
 }
---
+++ tumblingtoads/src/Blogs.js
@@ -0,0 +1,29 @@
+function Blogs(props) {
+    const blogs = props.posts.map((post) => {
+        return (
+            <div>
+                <div className="blog-preview-container" onClick={() => props.notifyParent(post)} key={post.name}>
+                    <div className="blog-preview-metadata">
+                        <div className="blog-preview-metadata-date">
+                            {post.year}
+                        </div>
+                        <div className="blog-preview-metadata-title">
+                            {post.name}
+                        </div>
+                        <div className="blog-preview-metadata-desc">
+                            {post.description}
+                        </div>
+                    </div>
+                    <img  className="blog-preview-thumbnail" src={post.thumbnail}></img>
+                </div>
+            </div>
+        )
+    });
+    return (
+        <div className="blog-content">
+           {blogs}
+        </div>
+    );
+}
+
+export default Blogs;
